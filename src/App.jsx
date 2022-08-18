cf21b7a02b00ae900e0f74fc2a63e69f49c503f0
Manunchaya Chongsutakawewong <sung478@hotmail.com>
2022-08-19 02:31:21 +0700

    create-home-router-edit-home-button

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import NavBar from './components/NavBar/NavBar'
 
+import Home from './pages/Home/Home'
 import Dashboard from './pages/Dashboard/Dashboard'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <Routes>
+          <Route path='/' element={<Home/>} />
           <Route path='/dashboard' element={<Dashboard/>} />
         </Routes>
       </BrowserRouter>
