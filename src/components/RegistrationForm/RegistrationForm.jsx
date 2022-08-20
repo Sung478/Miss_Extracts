2fc0fccd0b242ba6f32402b31b44acf8c47d1f8b
Manunchaya Chongsutakawewong <sung478@hotmail.com>
2022-08-20 21:54:50 +0700

    edit-registration-css

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -21,30 +21,30 @@ export default function RegistrationForm() {
       <form onSubmit={handleSubmit(console.log)}>
         <h2>Create Account</h2>
         <input {...register('username', {required: true, minLength: 6, maxLength: 15})} id='username' placeholder='Username' /><br/>
-        {errors.username && errors.username.type === 'required' && <span>Invalid or empty username.</span>}
-        {errors.username && (errors.username.type === 'minLength' || errors.username.type === 'maxLength') && <span>Username should be between 6-15 characters.</span>}
+        {errors.username && errors.username.type === 'required' && <span>Invalid or empty username.<br/></span>}
+        {errors.username && (errors.username.type === 'minLength' || errors.username.type === 'maxLength') && <span>Username should be between 6-15 characters.<br/></span>}
 
         <input {...register('name', {required: true, minLength: 1, maxLength: 50})} id='name' placeholder='Name' /><br/>
-        {errors.name && errors.name.type === 'required' && <span>What's your name?</span>}
-        {errors.name && (errors.name.type === 'minLength' || errors.name.type === 'maxLength') && <span>Name should be between less than 50 characters.</span>}
+        {errors.name && errors.name.type === 'required' && <span>What's your name?<br/></span>}
+        {errors.name && (errors.name.type === 'minLength' || errors.name.type === 'maxLength') && <span>Name should be between less than 50 characters.<br/></span>}
 
         <input {...register('email', {required: true})} type='email' placeholder='email' /><br/>
-        {errors.email && errors.email.type === 'required' && <span>Please enter a valid email.</span>}
+        {errors.email && errors.email.type === 'required' && <span>Please enter a valid email.<br/></span>}
 
         <input {...register('password', {required: true, minLength: 8, maxLength: 16})} id='password' type="password" placeholder="Password" /><br/>
-        {errors.password && errors.password.type === 'required' && <span>Password is required</span>}
-        {errors.password && (errors.password.type === 'minLength' || errors.password.type === 'maxLength') && <span>Your password must be between 8-16 characters.</span>}
+        {errors.password && errors.password.type === 'required' && <span>Password is required<br/></span>}
+        {errors.password && (errors.password.type === 'minLength' || errors.password.type === 'maxLength') && <span>Your password must be between 8-16 characters.<br/></span>}
 
         <input {...register('dateOfBirth', {required: true})} id='dateOfBirth' type="date" placeholder="Date of birth" /><br/>
 
         <input {...register('height', {required: true})} id='height' type="number" placeholder="Height(cm)" /><br/>
-        {errors.height && errors.height.type === 'required' && <span>What's your height?</span>}
+        {errors.height && errors.height.type === 'required' && <span>What's your height?<br/></span>}
 
         <input {...register('weight', {required: true})} id='weight' type="number" placeholder="Weight(kg)" /><br/>
-        {errors.weight && errors.weight.type === 'required' && <span>What's your weight?</span>}
+        {errors.weight && errors.weight.type === 'required' && <span>What's your weight?<br/></span>}
 
 
-        <input type="submit" />
+        <input id='register-button' type="submit" />
       </form>
     </div>
   );
--- src/pages/Regitration/Registration.jsx
+++ src/pages/Regitration/Registration.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import './Registration.css'
+
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm'
 
 export default function Registration() {
   return (
     <div className='registration'>
         <RegistrationForm />
+        <div id='registration-img'>
+            <img src='/registrationPic.svg' />
+        </div>
     </div>
   )
 }
