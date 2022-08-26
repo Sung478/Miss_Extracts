8aab1d7d018e0132e0af24e3597db99ef5b3060c
Manunchaya Chongsutakawewong <sung478@hotmail.com>
2022-08-26 11:34:39 +0700

    MyActivity

--- src/pages/MyActivities/MyActivities.jsx
+++ src/pages/MyActivities/MyActivities.jsx
@@ -13,71 +13,71 @@ export default function MyActivities() {
     const [isLoading, setIsLoading] = useState(true);
     const [user, setUser] = useState(
         {
-            userId: '1',
-            name: 'Sung',
-            username: 'username',
-            email: 'sung000@hotmail.com',
-            password: '000000000',
-            profilePic: 'https://www.figma.com/file/czpxRx46XfXd4IFIKll6kx/Untitled?node-id=68%3A2007',
-            birth: '2Jan20',
-            height: 170,
-            weight: 55,
-            goal: {
-                weeklyGoal: 4,
-                weightGoal: 49,
-                inspiration: "ware kid's clothhh and dance all night",
-            },
-            doneWeekly: 4,
-            activities: [
-                { 
-                    activityId: '1',
-                    activityType: 'cardio',
-                    activityName: 'running',
-                    date: "2022-07-08",
-                    duration: 30,
-                    comment: 'nice vibe'
-                },
-                { 
-                    activityId: '2',
-                    activityType: 'cardio',
-                    activityName: 'running',
-                    date: "2022-07-08",
-                    duration: 30,
-                    comment: 'nice vibe'
-                },
-                { 
-                    activityId: '3',
-                    activityType: 'cardio',
-                    activityName: 'running',
-                    date: "2022-07-08",
-                    duration: 30,
-                    comment: 'nice vibe'
-                },
-                { 
-                    activityId: '4',
-                    activityType: 'cardio',
-                    activityName: 'running',
-                    date: "2022-07-08",
-                    duration: 30,
-                    comment: 'nice vibe'
-                },
-                { 
-                    activityId: '5',
-                    activityType: 'cardio',
-                    activityName: 'running',
-                    date: "2022-07-08",
-                    duration: 30,
-                    comment: 'nice vibe'
-                },
-                { 
-                    activityId: '6',
-                    activityType: 'cardio',
-                    activityName: 'running',
-                    date: "2022-07-08",
-                    duration: 30,
-                    comment: 'nice vibe'
-                }
-            ]
+            // userId: '',
+            // name: '',
+            // username: 'username',
+            // email: 'sung000@hotmail.com',
+            // password: '000000000',
+            // profilePic: 'https://www.figma.com/file/czpxRx46XfXd4IFIKll6kx/Untitled?node-id=68%3A2007',
+            // birth: '2Jan20',
+            // height: 170,
+            // weight: 55,
+            // goal: {
+            //     weeklyGoal: 4,
+            //     weightGoal: 49,
+            //     inspiration: "ware kid's clothhh and dance all night",
+            // },
+            // doneWeekly: 4,
+            // activities: [
+            //     { 
+            //         activityId: '1',
+            //         activityType: 'cardio',
+            //         activityName: 'running',
+            //         date: "2022-07-08",
+            //         duration: 30,
+            //         comment: 'nice vibe'
+            //     },
+            //     { 
+            //         activityId: '2',
+            //         activityType: 'cardio',
+            //         activityName: 'running',
+            //         date: "2022-07-08",
+            //         duration: 30,
+            //         comment: 'nice vibe'
+            //     },
+            //     { 
+            //         activityId: '3',
+            //         activityType: 'cardio',
+            //         activityName: 'running',
+            //         date: "2022-07-08",
+            //         duration: 30,
+            //         comment: 'nice vibe'
+            //     },
+            //     { 
+            //         activityId: '4',
+            //         activityType: 'cardio',
+            //         activityName: 'running',
+            //         date: "2022-07-08",
+            //         duration: 30,
+            //         comment: 'nice vibe'
+            //     },
+            //     { 
+            //         activityId: '5',
+            //         activityType: 'cardio',
+            //         activityName: 'running',
+            //         date: "2022-07-08",
+            //         duration: 30,
+            //         comment: 'nice vibe'
+            //     },
+            //     { 
+            //         activityId: '6',
+            //         activityType: 'cardio',
+            //         activityName: 'running',
+            //         date: "2022-07-08",
+            //         duration: 30,
+            //         comment: 'nice vibe'
+            //     }
+            // ]
         }
     )
     const [currentItems, setCurrentItems] = useState([]);
