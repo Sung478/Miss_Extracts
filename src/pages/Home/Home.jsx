d178b6f3646c8bd6a75ed1d09faaada75a64e310
Manunchaya Chongsutakawewong <sung478@hotmail.com>
2022-08-29 00:33:38 +0700

    home checking

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,19 +9,27 @@ import { useState, useEffect } from 'react'
 
 export default function Home() {
   const [isSignin, setIsSignin] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
 
   const home = true;
 
+  const checkStatus = () => {
+    axiosInstance.get('/user_id')
+    .then(response => {
+        console.clear()
+        console.log(`account: @${response.data.username}`)
+    }).then(() => setIsSignin(true)).then(() => setIsLoading(false) )
+    .catch(() => {
+        console.log('Not sign in')
+        setIsLoading(false)
+    })
+  }
+
   useEffect(() => {
-      axiosInstance.get('/user_id')
-          .then(response => {
-              console.clear()
-              console.log(`account: @${response.data.username}`)
-          }).then(() => setIsSignin(true))
-          .catch(() => {
-              console.log('Not sign in')
-          })
+    checkStatus()
   },[])
+
+  if(isLoading) return <h3>Loading...</h3>
 
   return (
     <div className='home'>
