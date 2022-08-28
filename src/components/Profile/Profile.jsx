77e0a1f68e0807f9881a5e08437e2c78d593708d
Manunchaya Chongsutakawewong <sung478@hotmail.com>
2022-08-28 23:44:04 +0700

    date formate

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -14,7 +14,7 @@ export default function Profile({user, reload, toggleModal}) {
         <p className="userName">{'@'+ user.username }</p>
         <div className="profileDetails">
           <p className="name">Name : {user.name || '-'}</p>
-          <p className="birthDay">Birth date : {new Date(user.birthDate).toLocaleDateString() || ''} </p>
+          <p className="birthDay">Birth date : {new Date(user.birthDate).toDateString() || ''} </p>
           <p className="age">age : {Math.floor((new Date() - new Date(user.birthDate))/86400000/365) || '-'} years</p>
           <p className="weigth">weight : {user.weight || '-'}</p>
           <p className="height">height : {user.height || '-'}</p>
