d33cf93cee3265011c731762f179659802caa32a
Manunchaya Chongsutakawewong <sung478@hotmail.com>
2022-08-28 22:23:21 +0700

    fix bmi

--- src/components/BMI/BMI.jsx
+++ src/components/BMI/BMI.jsx
@@ -4,6 +4,9 @@ import './BMI.css'
 export default function BMI(props) {
 
     let bmi = Math.floor((props.weight)/ ((props.height)/100)*((props.height)/100))
+    console.log(props.weight)
+    console.log(props.height)
+    console.log(bmi)
 
   return (
     <div className='bmi'>
